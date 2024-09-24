3346fc6bb57076dcf465f8e6e6d8b442439b1401
arcctg <91179695+arcctg@users.noreply.github.com>
2024-09-24 23:13:10 +0300

    Stage 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input string:");
+
+        String line = scanner.nextLine();
+
+        List.of(line.split("")).forEach(e -> System.out.print(e + " "));
     }
 }
