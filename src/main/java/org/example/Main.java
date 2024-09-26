5980c7039b9cae56d94aae2b166c5b7ecfb758b1
arcctg <91179695+arcctg@users.noreply.github.com>
2024-09-26 14:10:09 +0300

    Stage 3 v2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,26 +21,18 @@ public class Main {
         StringBuilder builder = new StringBuilder();
 
         for (int i = 0; i < binary.length(); i++) {
+            char c = binary.charAt(i);
             int gg = 0;
 
-            switch (binary.charAt(i)) {
-                case '1' -> {
-                    for (int j = i + 1; j < binary.length(); j++) {
-                        if (binary.charAt(j) == '1') gg++;
-                        else break;
-                    }
+            builder.append(binary.charAt(i) == '1' ? "0 " : "00 ");
 
-                    builder.append("0 0%s ".formatted("0".repeat(gg)));
-                }
-                case '0' -> {
-                    for (int j = i + 1; j < binary.length(); j++) {
-                        if (binary.charAt(j) == '0') gg++;
-                        else break;
-                    }
-
-                    builder.append("00 ").append("0".repeat(1 + gg)).append(" ");
-                }
+            for (int j = i + 1; j < binary.length(); j++) {
+                if (binary.charAt(j) == c) gg++;
+                else break;
             }
+
+            builder.append("0%s ".formatted("0".repeat(gg)));
+
             i += gg;
         }
 
