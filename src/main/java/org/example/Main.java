40ff61966390a79cae41f8a8a0a6a2e13b459cfe
arcctg <91179695+arcctg@users.noreply.github.com>
2024-09-30 11:16:09 +0300

    Stage 4 v1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,63 @@
 package org.example;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String binary;
+        String binary, str;
 
-        System.out.println("Input string:");
+        System.out.println("Input encoded string:");
 
-        binary = toBinary(scanner.nextLine());
+        str = scanner.nextLine();
 
         System.out.println("\nThe result:");
 
-        System.out.println(encode(binary));
+        System.out.println(decode(str));
+    }
+
+    private static StringBuilder decode(String str) {
+        StringBuilder binary = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
+        char c;
+        int cntr;
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            c = str.charAt(++i);
+            cntr = 0;
+
+            for (i += (c == '0' ? 2 : 1); i < str.length(); i++) {
+                if (str.charAt(i) == '0') cntr++;
+                else break;
+            }
+
+            binary.append((c == '0' ? "0" : "1").repeat(cntr));
+        }
+
+        for (String s : binary.toString().split("(?<=\\G.{7})")) {
+            builder.append((char) Integer.parseInt(s, 2));
+        }
+
+        return builder;
     }
 
     private static StringBuilder encode(String binary) {
         StringBuilder builder = new StringBuilder();
+        char c;
+        int cntr;
 
         for (int i = 0; i < binary.length(); i++) {
-            char c = binary.charAt(i);
-            int gg = 0;
-
-            builder.append(binary.charAt(i) == '1' ? "0 " : "00 ");
+            c = binary.charAt(i);
+            cntr = 0;
 
             for (int j = i + 1; j < binary.length(); j++) {
-                if (binary.charAt(j) == c) gg++;
+                if (binary.charAt(j) == c) cntr++;
                 else break;
             }
 
-            builder.append("0%s ".formatted("0".repeat(gg)));
+            builder.append(c == '1' ? "0 " : "00 ").append("0%s ".formatted("0".repeat(cntr)));
 
-            i += gg;
+            i += cntr;
         }
 
         return builder;
@@ -41,9 +65,10 @@ public class Main {
 
     private static String toBinary(String line) {
         StringBuilder binary = new StringBuilder();
-        line.chars().forEach(e -> {
-            binary.append(String.format("%07d", new BigInteger(Integer.toBinaryString(e))));
-        });
+
+        for (char c : line.toCharArray()) {
+            binary.append(String.format("%07d", Integer.parseInt(Integer.toBinaryString(c))));
+        }
 
         return binary.toString();
     }
